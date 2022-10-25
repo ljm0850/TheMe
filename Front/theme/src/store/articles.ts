4626f1782800676c06a8c61cb66197fc09b971d2
leejaemin <dlwoals0850@gmail.com>
2022-10-25 17:41:20 +0900

    setup: store 세분화 #S07P31C203-32

---
+++ Front/theme/src/store/articles.ts
@@ -0,0 +1,18 @@
+// import router from '@/router'
+// import axios from 'axios'
+// import rest from '@/api/rest'
+
+export default {
+    state: {
+
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    }
+}
