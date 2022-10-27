810baac84c054cf9a3674b3e174bf0603bcf21df
leejaemin <dlwoals0850@gmail.com>
2022-10-27 13:11:58 +0900

    feat: 로그인 페이지 수정, 카카오로그인은 10.28에 협의 후 진행

--- Front/theme/src/API/rest.ts
+++ Front/theme/src/API/rest.ts
@@ -1,6 +1,17 @@
 const HOST = ""
 
+
+// 카카오
+const KAKAO_Host = "https://kauth.kakao.com"
+const REST_API_KEY = "bab0a08f8b68900521759c285635e38a"
+const REDIRECT_URI = "http://localhost:8080/"
+const KAKAO_GET_AUTHORIZATION = `/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`
 export default {
+    kakao: {
+        get_auth: () => KAKAO_Host + KAKAO_GET_AUTHORIZATION,
+        login: () => KAKAO_Host,
+    },
+
     accounts: {
         
     }
