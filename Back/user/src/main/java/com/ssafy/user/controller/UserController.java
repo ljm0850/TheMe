13d12035bbb709176ce4d02aa0c900e5d3893cf3
김세진 <rlatpwls15@naver.com>
2022-10-29 14:58:53 +0900

    [03] feat : 테마 팔로우 및 팔로우 취소 #S07P31C203-41

--- Back/user/src/main/java/com/ssafy/user/controller/UserController.java
+++ Back/user/src/main/java/com/ssafy/user/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.ssafy.user.controller;
 
 import com.ssafy.user.dto.UserLoginDto;
+import com.ssafy.user.service.FollowService;
 import com.ssafy.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -18,9 +20,11 @@ public class UserController {
     private final static String OK = "success";
     private final static String FAIL = "fail";
     UserService userService;
+    FollowService followService;
     @Autowired
-    UserController(UserService userService){
+    UserController(UserService userService, FollowService followService){
         this.userService = userService;
+        this.followService = followService;
     }
     @PostMapping("/user/login")
     public ResponseEntity<?> loginUser(HttpServletResponse response, @RequestBody UserLoginDto userLoginDto){
@@ -36,4 +40,39 @@ public class UserController {
         }
         return new ResponseEntity<>(result,status);
     }
+
+    @PostMapping("/user/follow/{theme_id}/{user_id}/{target_user_id}")
+    public ResponseEntity<?> followTheme(HttpServletResponse response, @PathVariable(name = "theme_id") int theme_id, @PathVariable(name = "user_id") int user_id,
+                                            @PathVariable(name = "target_user_id") int target_user_id) {
+        Map<String, Object> result = new HashMap<>();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        try {
+            followService.followTheme(theme_id, user_id, target_user_id);
+            result.put("message",OK);
+            status = HttpStatus.OK;
+        } catch (Exception e) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            result.put("message",FAIL);
+        }
+
+        return new ResponseEntity<>(result,status);
+    }
+
+    @PostMapping("/user/follow/{follow_id}")
+    public ResponseEntity<?> cancelFollow(HttpServletResponse response, @PathVariable int follow_id) {
+        Map<String, Object> result = new HashMap<>();
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        try{
+            followService.cancelFollow(follow_id);
+            result.put("message", OK);
+            status = HttpStatus.OK;
+        } catch (Exception e) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            result.put("message",FAIL);
+        }
+        return new ResponseEntity<>(result, status);
+    }
+
+
 }
