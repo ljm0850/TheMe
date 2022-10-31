4bcc903452e761e157ad4521e1656b593473d4bf
leejaemin <dlwoals0850@gmail.com>
2022-10-31 14:11:08 +0900

    feat : F01 axios요청 함수

--- Front/theme/src/store/index.ts
+++ Front/theme/src/store/index.ts
@@ -1,10 +1,10 @@
 import { createStore } from "vuex";
 import createPersistedState from "vuex-persistedstate";
-import accounts from "./accounts";
-import articles from "./articles";
-import follow_like from "./follow_like";
-import report from "./report";
-import themes from "./themes";
+import accounts from "./acounts/accounts";
+import articles from "./articles/articles";
+import follow_like from "./follow_like/follow_like";
+import report from "./report/report";
+import themes from "./themes/themes";
 
 export default createStore({
     modules: { accounts,articles,follow_like,report,themes },
