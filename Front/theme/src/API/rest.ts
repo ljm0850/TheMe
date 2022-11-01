93fbae42c125d84f2ee17cbb2b9afbd706ac7b11
leejaemin <dlwoals0850@gmail.com>
2022-11-01 17:28:36 +0900

    feat: 게시물 사진 캐러셀 변경

--- Front/theme/src/API/rest.ts
+++ Front/theme/src/API/rest.ts
@@ -13,8 +13,8 @@ const SEARCH = '/search'
 // 카카오
 const KAKAO_Host = "https://kauth.kakao.com"
 const REST_API_KEY = "bab0a08f8b68900521759c285635e38a"
-// const REDIRECT_URI = "http://localhost:8080"
-const REDIRECT_URI = "http://k7c2031.p.ssafy.io:8080"
+const REDIRECT_URI = "http://localhost:8080"
+// const REDIRECT_URI = "http://k7c2031.p.ssafy.io:8080"
 const KAKAO_GET_AUTHORIZATION = `/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
 // https://kauth.kakao.com/oauth/authorize?client_id=bab0a08f8b68900521759c285635e38a&redirect_uri=http://localhost:8080&response_type=code HTTP/1.1
 export default {
