0b2f827cfed5053cd310057e74cb63ac7a15abc9
lion1735 <lion1735@naver.com>
2022-11-02 17:41:33 +0900

    fix : getThemeName Response 제거 #S07P31C203-78

--- Back/theme/src/main/java/com/ssafy/theme/controller/ThemeController.java
+++ Back/theme/src/main/java/com/ssafy/theme/controller/ThemeController.java
@@ -175,8 +175,8 @@ public class ThemeController {
         return new ResponseEntity<>(result, status);
     }
 
-    @GetMapping("/{theme_idx}/name")
-    public String getThemeName(HttpServletResponse response, @PathVariable(name = "theme_idx") int theme_idx) {
+    @GetMapping("/{name/theme_idx}")
+    public String getThemeName(@PathVariable(name = "theme_idx") int theme_idx) {
         return themeService.getThemeName(theme_idx);
     }
 }
