9f5c3f9737ff5f931681227ede07b911cf902ccc
opal18 <opal18@naver.com>
2022-11-03 15:34:47 +0900

    feat : 메인피드 지역 목록 DTO 추가 #S07P31C203-81

--- Back/feed/src/main/java/com/ssafy/feed/controller/FeedController.java
+++ Back/feed/src/main/java/com/ssafy/feed/controller/FeedController.java
@@ -1,24 +1,47 @@
 package com.ssafy.feed.controller;
 
+import com.ssafy.feed.dto.board.BoardListDto;
+import com.ssafy.feed.dto.comment.CommentListDto;
+import com.ssafy.feed.service.FeedService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
+@RequestMapping("/feed")
 public class FeedController {
     private final static String OK = "success";
     private final static String FAIL = "fail";
-
+    FeedService feedService;
     @Autowired
-    FeedController(){
-
+    FeedController(FeedService feedService){
+        this.feedService = feedService;
     }
-
+    @GetMapping("/region")
+    @ApiOperation(value = "메인피드 지역 목록" , notes = "0 : 전국, 1 : 서울, 2 : 대전, 3 : 광주, 4 : 구미, 5 : 부울경(부산,울산,경남)")
+    public ResponseEntity<?> feedByRegion(@RequestParam(name = "region") int region, HttpServletRequest request,
+                                          @RequestParam(name="pageSize") int pageSize, @RequestParam(name ="pageIdx")int pageIdx) {
+        Map<String, Object> result = new HashMap<>();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        //int userIdx = (int) request.getAttribute("userIdx");
+        int userIdx = 5;
+        try {
+            //feedService.feedByRegion(userIdx,region,pageIdx,pageSize); // 불러와야해요 리스트
+            result.put("message", OK);
+            status = HttpStatus.OK;
+        } catch (Exception e) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            result.put("message", FAIL);
+        }
+        return new ResponseEntity<>(result, status);
+    }
 }
