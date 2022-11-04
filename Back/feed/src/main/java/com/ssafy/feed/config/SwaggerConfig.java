0c55e8e78a11bf61018b9e3e68d247a8ea776b21
opal18 <opal18@naver.com>
2022-11-04 11:42:09 +0900

    feat : request 담기 위해 swagger 설정 #S07P31C203-112

--- Back/feed/src/main/java/com/ssafy/feed/config/SwaggerConfig.java
+++ Back/feed/src/main/java/com/ssafy/feed/config/SwaggerConfig.java
@@ -2,27 +2,45 @@ package com.ssafy.feed.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
     public Docket restAPI() {
+
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        List<Parameter> parameters = new ArrayList<>();
+
+        parameterBuilder
+                .name("userIdx")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+        parameters.add(parameterBuilder.build());
+
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(parameters)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ssafy"))
                 .paths(PathSelectors.any())
                 .build();
     }
-
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("The Me. THEME API SERVER")
