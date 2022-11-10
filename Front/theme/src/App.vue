38f00e2c141f83378edf39c7e648503896e7a48b
lion1735 <lion1735@naver.com>
2022-11-10 14:30:03 +0900

    style : 폰트 주아체#S07P31C203-145

--- Front/theme/src/App.vue
+++ Front/theme/src/App.vue
@@ -42,15 +42,15 @@ export default defineComponent({
   max-height: 844px;
   // max-width: 370px !important;
     // justify-content: center;    
-    font-family: 'GangwonEdu_OTFBoldA';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEdu_OTFBoldA.woff') format('woff');
+     font-family: 'BMJUA';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff') format('woff');
     font-weight: normal;
     font-style: normal;
     
 }
 @font-face {
-    font-family: 'GangwonEdu_OTFBoldA';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEdu_OTFBoldA.woff') format('woff');
+     font-family: 'BMJUA';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff') format('woff');
     font-weight: normal;
     font-style: normal;
 }
