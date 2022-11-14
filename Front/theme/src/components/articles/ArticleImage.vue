25403fb50d449fce9ef5276beba53f9cb321e819
leejaemin <dlwoals0850@gmail.com>
2022-11-14 11:06:13 +0900

    feat: 사진 캐러셀 적용, ArticleDetail에서 캐러셀 id 가져와야함

--- Front/theme/src/components/articles/ArticleImage.vue
+++ Front/theme/src/components/articles/ArticleImage.vue
@@ -1,66 +1,51 @@
 <template>
-<div v-if="pictures">
-    <div  :id="`${pictures}`" class="carousel slide" data-bs-ride="true">
-        <div class="carousel-indicators">
-            <button type="button"  v-for="picture,i in pictures" :key="i" :data-bs-target="`#${picture}`" 
-            :data-bs-slide-to=i :class="[i==0 ? 'active' : '']" aria-current="true"  >
-            </button>
-        </div>
-    </div>
-    <div class="carousel-inner">
-        <div v-for= "picture,i in pictures" :key ="i" class="carousel-item active">
-            <img :src="picture" alt="articleDetail"  style="width:100%">
-        </div>
-        <!-- <div class="carousel-item active">
-            <img src="@/assets/image/article.jpg" alt="articleDetail" style="width:100%">
-        </div> -->
-    </div>
-    
-</div>
-<!-- <div :id="`${pictures}`" class="carousel slide" data-bs-ride="true">
+<div v-if="pictures" :id="`${carouselId}`" class="carousel slide" data-bs-ride="true">
     <div class="carousel-indicators">
-        <button type="button" :data-bs-target="`#${pictures}`" data-bs-slide-to="0" class="active"
+        <button type="button" :data-bs-target="`#${carouselId}`" data-bs-slide-to="0" class="active"
             aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" :data-bs-target="`#${pictures}`" data-bs-slide-to="1"
-            aria-label="Slide 2"></button>
-        <button type="button" :data-bs-target="`#${pictures}`" data-bs-slide-to="2"
-            aria-label="Slide 3"></button>
+        <button v-for="idx in numArray" :key="idx" type="button" :data-bs-target="`#${carouselId}`" :data-bs-slide-to="idx"
+            :aria-label="`Slide {idx}`"></button>
     </div>
     <div  class="carousel-inner">
         <div class="carousel-item active">
-            <img src="@/assets/image/article.jpg" alt="articleDetail" style="width:100%">
+            <img :src="pictures[0]" alt="articleDetail" style="width:100%">
         </div>
-        <div class="carousel-item">
-            <img src="@/assets/image/article.jpg" alt="articleDetail" style="width:100%">
-        </div>
-        <div class="carousel-item">
-            <img src="@/assets/image/article.jpg" alt="articleDetail" style="width:100%">
+        <div v-for="idx in numArray" :key="idx" class="carousel-item">
+            <img :src="pictures[idx]" alt="articleDetail" style="width:100%">
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" :data-bs-target="`#${pictures}`"
+    <button class="carousel-control-prev" type="button" :data-bs-target="`#${carouselId}`"
         data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" :data-bs-target="`#${pictures}`"
+    <button class="carousel-control-next" type="button" :data-bs-target="`#${carouselId}`"
         data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
-</div> -->
+</div>
 </template>
-<!-- <img src="@/assets/image/article.jpg" alt="articleDetail" style="width:100%"></img> -->
-    <!-- <img src="@/assets/image/article.jpg" alt="articleDetail" style="width:300px"> -->
 
 <script lang="ts">
 // import { useStore } from "vuex";
 export default {
     props: {
-        pictures : Array
+        pictures: Array,
+        carouselId:String
     },
     components: {
     },
-    setup() {
+    setup(props:any) {
+        // eslint-disable-next-line vue/no-setup-props-destructure
+        const photoLength:number | undefined = props.pictures?.length
+        const numArray: number[] = [];
+        if (photoLength) {
+            for (let idx = 1; idx < photoLength; idx++){
+                numArray.push(idx)
+            }
+        }
+        return { numArray}
         
     }
 }
