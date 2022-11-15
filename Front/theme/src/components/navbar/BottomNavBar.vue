13666bc6c455f6554788e6c5d5486412f87a034a
lion1735 <lion1735@naver.com>
2022-11-15 15:21:59 +0900

    style : 하단바 디자인 수정 #S07P31C203-148

--- Front/theme/src/components/navbar/BottomNavBar.vue
+++ Front/theme/src/components/navbar/BottomNavBar.vue
@@ -18,22 +18,22 @@
     
     <div class="d-flex flex-row bg-white bottomeNav" >
       <router-link @mousedown.left ="mouseLeft" :to="{ name: 'Main' }"> 
-        <button type="button" class="button-size"><img src="@/assets/image/homeIcon.svg" alt="" class="icon"></button>
+        <button type="button" :class="[!state.plusButton ? 'button-size active':'button-size']"><img src="@/assets/image/homeIcon.svg" alt="" class="icon"></button>
         
       </router-link>  
       
       <router-link @mousedown.left ="mouseLeft" :to="{ name: 'ThemeList' }">
-        <button type="button" class="button-size"><img src="@/assets/image/mapIcon.svg" alt="" class="icon"></button>
+        <button type="button" :class="[!state.plusButton ? 'button-size active':'button-size']"><img src="@/assets/image/mapIcon.svg" alt="" class="icon"></button>
       </router-link>
     
-      <button type="button" @click.prevent="togglePlusButton()" class="button-size "><img src="@/assets/image/plusIcon.svg" alt="" class="icon"></button>
+      <button type="button" @click.prevent="togglePlusButton()" :class="[!state.plusButton ? 'button-size':'button-size plus-icon']"><img src="@/assets/image/plusIcon.svg" alt="" class="icon"></button>
     
       <router-link @mousedown.left ="mouseLeft" :to="{ name: 'Search' }">
-        <button type="button" class="button-size"><img src="@/assets/image/searchIcon.svg" alt="" class="icon"></button>
+        <button type="button" :class="[!state.plusButton ? 'button-size active':'button-size']"><img src="@/assets/image/searchIcon.svg" alt="" class="icon"></button>
       </router-link>
     
       <router-link @mousedown.left ="mouseLeft"  ter-link :to="{ name: 'Profile', params: { nickname: myNickname } }">
-        <button type="button" class="button-size" ><img src="@/assets/image/profileIcon.svg" alt="" class="icon"></button>
+        <button type="button" :class="[!state.plusButton ? 'button-size active':'button-size']" ><img src="@/assets/image/profileIcon.svg" alt="" class="icon"></button>
       </router-link>
     </div>
   </nav>
@@ -67,12 +67,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.plus-icon{
+  // background-color: #d3f6d1 !important; 
+  background-color: #fbe9d2 !important;
+}
+.plus-icon img{
+  // filter: invert(92%) sepia(5%) saturate(6145%) hue-rotate(54deg) brightness(76%) contrast(92%) !important;    
+  filter: invert(79%) sepia(36%) saturate(3460%) hue-rotate(335deg) brightness(100%) contrast(83%) !important;
+}
 .btn-lg{
   border: 0px solid #E89A3D;
-  background-color: #fbe9d2;
+  // background-color: #fbe9d2;
+  background-color: #d3f6d1;
   font-size: 14px;
-  color: #000;
+  // color: #E89A3D;
+  color: #48bd42;
   margin-bottom: 4px;
 }
 .bottomeNav{
@@ -80,7 +89,7 @@ export default {
 }
   .navbar-position {
     position: fixed;
-    bottom: 0;
+    bottom: -1px;
     z-index: 150;
   }
   .button-size {
@@ -98,10 +107,13 @@ export default {
     width: 20px;
     height: 20px;
   }
-  .bottomeNav .router-link-active button{
+  .bottomeNav .router-link-active .active{
     background-color: #fbe9d2;
+    // background-color: #d3f6d1;
   }
-  .bottomeNav .router-link-active button .icon{
+  .bottomeNav .router-link-active .active .icon{
     filter: invert(79%) sepia(36%) saturate(3460%) hue-rotate(335deg) brightness(100%) contrast(83%);
+    // filter: invert(77%) sepia(5%) saturate(5459%) hue-rotate(64deg) brightness(107%) contrast(69%);
+    // filter: invert(92%) sepia(5%) saturate(6145%) hue-rotate(54deg) brightness(76%) contrast(92%);
   }
 </style>
