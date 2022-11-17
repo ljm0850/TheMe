3874bc91aea04de0989cc571516a23ed9249b863
joe <gosmf12@naver.com>
2022-11-17 13:31:46 +0900

    feat : 댓글, 신고 기능 #S07P31C203-135

--- Front/theme/src/components/articles/comment/CommentItem.vue
+++ Front/theme/src/components/articles/comment/CommentItem.vue
@@ -1,33 +1,55 @@
 <template>
-    <div>
+    <div class="comment-wrap">
         <div class="commentContent">
-            <img :src="comment.profile" alt="" class="profileImg">
-            {{comment.nickname}} 
+            <div>
+                <img :src="comment.profile" alt="" class="comment-profileImg">
+                {{comment.nickname}}
+            </div>
+            <div>
+                <div v-if="!comment.writer" class="" @click.prevent="alertComment()">신고</div>
+                <div v-if="comment.writer" class="" @click.prevent="deleteComment(idx)">삭제</div>
+            </div>
+        </div>
+        <div>
             {{comment.content}} 
         </div>
-        <div class="commentContent">
-            <button @click.prevent="alertComment()">신고</button>
-            <button @click.prevent="deleteComment()">삭제</button>
-        </div>
+        <!-- <div class="commentContent">
+            
+        </div> -->
     </div>
 </template>
 
 <script lang="ts">
+import { reactive } from "vue";
 import { useStore } from "vuex";
 export default {
     props: {
-        comment:Object
+        comment:Object,
+        idx:Number
     },
     components: {
     },
-    setup(props:any) {
+    setup(props:any, {emit} : any ) {
         const store = useStore();
 
-        const deleteComment = () => {
+        const state : any= reactive({
+            commentIdx : props.comment.commentIdx,
+            content : "",
+            // commentList : {},
+        });
+
+        const deleteComment = (idx:number) => {
             store.dispatch("deleteComment", props.comment.commentIdx);
+            emit('triggerDelete', idx);
+            console.log(idx)
         }
+
         const alertComment = () => {
-            store.dispatch("reportComment", props.comment.commentIdx);
+            var input = prompt('댓글 신고', '신고 사유를 입력해 주세요.');
+            alert(input);
+            state.content = input;
+            store.dispatch("reportComment", state);
+            
         }
 
         return{ alertComment, deleteComment }
@@ -37,12 +59,26 @@ export default {
 
 <style lang="scss">
 
+
 .commentContent{
-    display: inline !important;
+    display: flex;
+    justify-content: space-between;
 }
 
-.profileImg{
+.comment-btn{
+    position:relative;
+    top : -10px;
+    left : 290px;
+}
+
+.comment-profileImg{
+    border-radius: 200px;
+    border: 2px;
     width: 20px;
+}
+
+.comment-wrap{
+    margin-top: 13px;
 }
 
 </style>
