936209cbdd6d0487d4f51b12b1bbaec8c343d313
ljm0850 <dlwoals0850@gmail.com>
2022-11-20 21:34:12 +0900

    fix:setTime 조절

--- Front/theme/src/store/feed/feed.ts
+++ Front/theme/src/store/feed/feed.ts
@@ -80,8 +80,9 @@ export default {
             })
                 .then((res) => {
                     // console.log(res.data)
-                    setTimeout(function(){},3000)
-                    router.push({name: 'UserTheme', params: { userThemeIdx:_data.themeIdx, publicThemeIdx:_data.publicThemeIdx }})
+                    setTimeout(function(){
+                        router.push({name: 'UserTheme', params: { userThemeIdx:_data.themeIdx, publicThemeIdx:_data.publicThemeIdx }})
+                    },3000)
             })
         },
 
